4269a7deb12762ced2a435fe81267f337b78a88f
Farid <76972056+Faro08@users.noreply.github.com>
2021-03-07 20:42:06 -0300

    hero terminado sin rwd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,38 @@ import styled from 'styled-components';
 
 import { Color__celeste, Color__celesteClaro} from '../styled';
 import banner from '../images/banner.png';
+import grua  from '../images/grua.png'; 
+
 
 const HomeHero = styled.div`
     height:970px; 
     background: url(${banner}); // la img tiene 1080 de alto
     padding-top: 110px;
 
-    h1{
-        text-align: center;
-        font-size:5em;
-        font-weight: lighter;
+    .Hero__container{
+        margin-top: 100px;
+        display: flex;
+        flex-direction: row;
+
+        .Hero__container-title{
+            width: 50%;
+    
+            h1{
+                text-align: center;
+                font-size:5em;
+                font-weight: lighter;
+            }
+        }
+
+        .Hero__container-img{
+            width:50%;
+            text-align: center;
+
+            img{
+                height: auto; //700px
+                width: 100%;
+            }
+        }
     }
 `;
 
@@ -42,8 +64,13 @@ function Home(){
     return(
         <React.Fragment>
             <HomeHero>
-                <div className="container">
-                    <h1>DESARROLLO<br/> DE SOFTWARE</h1>
+                <div className="Hero__container">
+                    <div className="Hero__container-title">
+                        <h1>DESARROLLO<br/> DE SOFTWARE</h1>
+                    </div>
+                    <div className="Hero__container-img">
+                        <img src={grua} alt="Grua"/>
+                    </div>
                 </div>
             </HomeHero>
             <HomeSectionTecno>
